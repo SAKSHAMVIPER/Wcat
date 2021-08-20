7a90ecca007eb3d3bceafe3781466a626752547a
saksham <sakshamsinghrkt387@gmail.com>
2021-08-20 09:56:52 +0530

    hello

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const { log } = require("console");
 const fs = require("fs");
 let arguments = process.argv.slice(2);
 
@@ -39,32 +40,47 @@ for(let file of filenames) {
         if(flag == "-rs") {
             fileData = removeAll(fileData," ");  //remove space
         }
-        if(flag == "-rn") {
+        if(flag == "-rn"){
             fileData = removeAll(fileData, "\r\n") // remove extra line
         }
         if(flag == "-rsc") {
-            for(let secondaryArgument of secondaryArguments) {
+            for(let secondaryArgument of secondaryArguments) {   //remove special character
                 fileData = removeAll(fileData,secondaryArgument);
             }
         }
         if(flag == "-s"){
-            addsequence(filedata);
+            let data = addsequence(fileData);
+            console.log(data);
         }
         if(flag == "-sn"){
-            addsequencenline(filedata);
+            let data = addsequencenline(fileData);
+            console.log(data);
         }
     }
-    console.log(fileData);
+    // console.log(fileData);
 }
 
 function removeAll(string, removalData) {
     return string.split(removalData).join("");
 }
-  function addsequence(string){
-
+  function addsequence(content){
+      let contentarr = content.split("\r\n");
+      for(let i = 0;i < contentarr.length;i++){
+         contentarr[i] = (i+1) + " " +contentarr[i];
+      }
+      return contentarr;
+       
   }
 
 
-  function addsequencenline(string){
-      
+  function addsequencenline(content){
+    let contentarr = content.split("\r\n");
+    let count = 1;
+    for(let i = 0;i < contentarr.length;i++){
+        if(contentarr[i] != ""){
+       contentarr[i] = count + " " +contentarr[i];
+       count++;
+        }
+    }
+    return contentarr;
 }
